644c8f41ef2908aebafaf51d291fd8621143456d
ksma19 <kusumadiana615@gmail.com>
2023-03-03 22:38:13 +0700

    adding favico; add & edit car

--- src/config/Router.js
+++ src/config/Router.js
@@ -4,12 +4,14 @@ import Login from "../pages/login/Login";
 import CanvasCar from "../Components/Dashboardlist/CanvasCar";
 import ReportTable from "../pages/DashboardContent/ReportTable";
 import CanvasDashboard from '../Components/Dashboardlist/CanvasDashboard';
+// import ChartComponent from "../pages/DashboardContent/ChartComponent";
+import AddCar from "../Components/Car/AddCar";
 import ChartComponent from "../pages/DashboardContent/ChartComponent";
 
 export const Router = createBrowserRouter([
     {
         path: "/",
-        element: <RenderLayout children={[<CanvasDashboard />]} />
+        element: <RenderLayout children={[< ReportTable />, <CanvasDashboard />]} />
     },
     {
         path: "/list",
@@ -17,6 +19,14 @@ export const Router = createBrowserRouter([
     },
     {
         path: "/SignIn",
+        element: <Login />
+    },
+    {
+        path: "/ChartComponent",
         element: <ChartComponent />
-    }
+    },
+    {
+        path: "/AddCar",
+        element: <RenderLayout children={[<AddCar />]} />
+    },
 ]);
